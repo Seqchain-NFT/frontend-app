916764f1fff5357f7e95d014d3349d048d4b9fc5
Den Kravchu <denkravchu@gmail.com>
2022-02-14 11:44:04 +0100

    finished first screen

--- sequoia/src/screens/Welcome/Welcome.jsx
+++ sequoia/src/screens/Welcome/Welcome.jsx
@@ -3,6 +3,7 @@ import { useRef, useEffect } from 'react'
 import './Welcome.scss'
 
 import groundImage from './assets/ground.png'
+import groundShadowImage from './assets/shadow.png'
 import nftCommonImage from './assets/nft-common.png'
 import nftRareImage from './assets/nft-rare.png'
 import nftLegendaryImage from './assets/nft-legendary.png'
@@ -24,22 +25,40 @@ const Welcome = () => {
     parallaxElements[1] = useRef()
     parallaxElements[2] = useRef()
 
-    const canvas = useRef()
-    const blurImages = [ blur1, blur2, blur3, blur4 ]
+    const canvas = {
+        dom: useRef(),
+        context: null,
+        width: 0,
+        height: 0,
+        blursCount: 60,
+        blurImages: [],
+        mouseRadius: 250,
+        ground
+    }
 
     useEffect(() => {
-        renderer.setToRender(groundParallax.bind(undefined, ground.current), 'groundParallax')
+        renderer.setToRender(groundParallax.bind(undefined, ground.current, canvas.dom.current), 'groundParallax')
         renderer.setToRender(welcomeMouseParallax.bind(undefined, ground.current, parallaxElements), 'welcomeMouseParallax')
 
-        const context = canvas.current.getContext('2d')
-        renderer.setToRender(canvasAnimation.bind(undefined, canvas.current, context, blurImages), 'canvasAnimation')
+        canvas.context = canvas.dom.current.getContext('2d')
+        setTimeout(function() {
+            canvas.blurImages = generateBlursImages.apply(canvas, [ blur1, blur2, blur3, blur4 ])
+        }, 100)
+        renderer.setToRender(canvasAnimation.bind(canvas), 'canvasAnimation')
+
+        return () => {
+            renderer.removeFromRender('groundParallax')
+            renderer.removeFromRender('welcomeMouseParallax')
+            renderer.removeFromRender('canvasAnimation')
+        }
     }, [])
 
     return (
         <div className="welcome">
             <div className="welcome__canvas">
+                <img className="welcome__canvas-shadow" src={groundShadowImage} alt="" />
                 <img ref={ground} className="welcome__canvas-ground" src={groundImage} alt=""/>
-                <canvas ref={canvas}></canvas>
+                <canvas ref={canvas.dom}></canvas>
                 <div className="welcome__canvas-nfts">
                     <img src={nftCommonImage} ref={parallaxElements[0]} className="nft-common" alt="" />
                     <img src={nftRareImage} ref={parallaxElements[1]} className="nft-rare" alt="" />
@@ -71,9 +90,10 @@ const Welcome = () => {
     )
 }
 
-function groundParallax(ground) {
+function groundParallax(ground, canvas) {
     if (renderer.isElementVisible(ground)) {
         ground.style.transform = `translate3d(0, ${window.scrollY / 5}px, 0)`
+        canvas.style.transform = `translate3d(0, ${window.scrollY / 8}px, 0)`
     }
 }
 
@@ -86,15 +106,92 @@ function welcomeMouseParallax(ground, domElements) {
     }
 }
 
-function canvasAnimation(canvas, context, blurImages) {
-    if (renderer.isElementVisible(canvas)) {
-
+function generateBlursImages(...blurUrls) {
+    const blurs = []
+    for (let i = 0; i < this.blursCount; i++) {
+        const img = new Image(50, 50)
+        const index = getRandomIntInclusive(0, blurUrls.length - 1)
+        img.src = blurUrls[index]
+        img.onload = () => { console.log('LOADED BLURED NFT'); blurs[i].loaded = true }
+        blurs.push({
+            img,
+            x: getRandomIntInclusive(0, window.innerWidth),
+            y: getRandomIntInclusive(0, this.dom.current.getBoundingClientRect().height - 100),
+            dir: getDirection(),
+            speed: 0.25,
+            loaded: false
+        })
     }
-    canvasAdaptive(canvas)
+    return blurs
 }
 
-function canvasAdaptive(canvas) {
+function getDirection() {
+    if (Math.random() <= 0.5) { return -1 }
+    return 1
+}
 
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function canvasAnimation() {
+    if (renderer.isElementVisible(this.dom.current)) {
+        const mouse = renderer.getMouseDocumentCoords()
+        this.context.clearRect(0, 0, this.width, this.height)
+        this.blurImages.forEach(blur => {
+            if (!blur.loaded) { return }
+
+            this.context.drawImage(blur.img, blur.x, blur.y, this.width / 30, this.width / 30)
+
+            blur.x += blur.speed * blur.dir
+            blur.y += blur.speed * blur.dir
+            if (blur.x <= -50 || blur.x >= this.width || blur.y <= -50 || blur.y >= this.height - 150 ) {
+                blur.dir *= -1
+            }
+
+            // move on hover
+            if ( (blur.x - mouse.x) ** 2 + (blur.y - mouse.y) ** 2 <= this.mouseRadius ** 2 ) {
+                let blurX = 0, blurY = 0
+                const deltaX = mouse.x - blur.x
+                const deltaY = mouse.y - blur.y
+                if ( Math.abs(deltaX) > Math.abs(deltaY) ) {
+                  if (deltaX > 0) {
+                    blurX = blur.x - 1
+                    blurY = ( -(mouse.x * blur.y - blur.x * mouse.y) - (mouse.y - blur.y) * blurX ) / ( blur.x - mouse.x )
+                  }
+                  if (deltaX <= 0) {
+                    blurX = blur.x + 1
+                    blurY = ( -(mouse.x * blur.y - blur.x * mouse.y) - (mouse.y - blur.y) * blurX ) / ( blur.x - mouse.x )
+                  }
+                } else {
+                  if (deltaY > 0) {
+                    blurY = blur.y - 1
+                    blurX = ( -(mouse.x * blur.y - blur.x * mouse.y) - (blur.x - mouse.x) * blurY ) / ( mouse.y - blur.y )
+                  }
+                  if (deltaY <= 0) {
+                    blurY = blur.y + 1
+                    blurX = ( -(mouse.x * blur.y - blur.x * mouse.y) - (blur.x - mouse.x) * blurY ) / ( mouse.y - blur.y )
+                  }
+                }
+                if (blurX <= -50 || blurX >= this.width || blurY <= -50 || blurY >= this.height - 150 ) { return }
+                blur.x = blurX
+                blur.y = blurY
+            }
+
+
+        })
+    }
+    canvasAdaptive.apply(this)
+}
+
+function canvasAdaptive() {
+    const canvas = this.dom.current.getBoundingClientRect()
+    if (this.width !== canvas.width || this.height !== canvas.height) {
+        this.width = canvas.width
+        this.height = canvas.height
+        this.dom.current.width = this.width
+        this.dom.current.height = this.height
+    }
 }
 
 export default Welcome
